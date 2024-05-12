1b28d1a3460dd1677b4e116d1b36c01a203098ef
Michael Simmons <msimmons.cr@gmail.com>
2024-05-12 19:58:28 -0300

    feat: CRUD funcionando

--- RankingPadel/partidos/models.py
+++ RankingPadel/partidos/models.py
@@ -17,16 +17,22 @@ class Jugador(models.Model):
         return f"{self.nombre} {self.apellido}"
     
 class Torneo(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
     fecha_de_inicio = models.DateField()
     fecha_de_cierre = models.DateField()
+
+    def __str__(self) -> str:
+        return self.nombre
     
 class Partido(models.Model):
-    jugador_local_1 = models.ForeignKey(Jugador, on_delete=models.SET_NULL, null=True, blank=True)
-    jugador_local_2 = models.ForeignKey(Jugador, on_delete=models.SET_NULL, null=True, blank=True)
+    jugador_local_1 = models.ForeignKey(Jugador, related_name='partidos_como_local_1', on_delete=models.SET_NULL, null=True, blank=True)
+    jugador_local_2 = models.ForeignKey(Jugador, related_name='partidos_como_local_2', on_delete=models.SET_NULL, null=True, blank=True)
     games_local = models.PositiveIntegerField()
     games_visitante = models.PositiveIntegerField()
-    jugador_visitante_1 = models.ForeignKey(Jugador, on_delete=models.SET_NULL, null=True, blank=True)
-    jugador_visitante_2 = models.ForeignKey(Jugador, on_delete=models.SET_NULL, null=True, blank=True)
+    jugador_visitante_1 = models.ForeignKey(Jugador, related_name='partidos_como_visitante_1', on_delete=models.SET_NULL, null=True, blank=True)
+    jugador_visitante_2 = models.ForeignKey(Jugador, related_name='partidos_como_visitante_2', on_delete=models.SET_NULL, null=True, blank=True)
     fecha_de_partido = models.DateTimeField()
-    torneo = models.ForeignKey(Torneo, on_delete=models.SET_NULL, null=True, blank=True)
+    torneo = models.ForeignKey('Torneo', related_name='partidos', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.jugador_local_1.nombre} & {self.jugador_local_2.nombre} Vs. {self.jugador_visitante_1.nombre} & {self.jugador_visitante_2.nombre}"
