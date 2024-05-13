6a3c045566733ef41f06a112528766b8f0eee5b4
Michael Simmons <msimmons.cr@gmail.com>
2024-05-12 21:00:58 -0300

    feat: core index y partidos index

---
+++ RankingPadel/core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "Core"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- RankingPadel/partidos/views.py
+++ RankingPadel/partidos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from . import models
+
+def index(request):
+    consulta = models.Partido.objects.all()
+    contexto = {"partidos": consulta}
+    return render(request, "partidos/index.html", contexto)
