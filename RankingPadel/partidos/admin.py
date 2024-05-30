88eb145efac18d43f39dfcf8720c2b35fef0832b
Michael Simmons <msimmons.cr@gmail.com>
2024-05-29 22:06:57 -0300

    bd

--- RankingPadel/partidos/admin.py
+++ RankingPadel/partidos/admin.py
@@ -4,6 +4,19 @@ from django.contrib import admin
 
 from . import models
 
+class PartidoAdmin(admin.ModelAdmin):
+    list_display = ("jugador_local_1",
+                    "jugador_local_2",
+                    "games_local",
+                    "games_visitante",
+                    "jugador_visitante_1",
+                    "jugador_visitante_2",
+                    "fecha_de_partido",
+                    "torneo")
+    list_display_links = ("jugador_local_1", "jugador_local_2", "jugador_visitante_1","jugador_visitante_2","torneo",)
+    list_filter = ("torneo",)
+    date_hierarchy = "fecha_de_partido"
+
 admin.site.register(models.Jugador)
 admin.site.register(models.Torneo)
-admin.site.register(models.Partido)
+admin.site.register(models.Partido, PartidoAdmin)
